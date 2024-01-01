fe1c95d84cc73d6091cf2972f5f7fb10523e6dd2
Eren <ernuygur@gmail.com>
2024-01-01 23:00:31 +0300

    feat(controller) changed the field injection

--- src/main/java/dev/erenuygur/flightsearchapi/controller/SecureJobController.java
+++ src/main/java/dev/erenuygur/flightsearchapi/controller/SecureJobController.java
@@ -2,16 +2,16 @@ package dev.erenuygur.flightsearchapi.controller;
 
 import dev.erenuygur.flightsearchapi.job.FlightScheduledJob;
 import io.swagger.v3.oas.annotations.Operation;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/secure")
 public class SecureJobController {
 
-    @Autowired
     private FlightScheduledJob flightScheduledJob;
 
     @Operation(summary = "Trigger the flight scheduled job manually.")
