a321081df1988b5f14fa89ff52640b6440506c51
Eren <ernuygur@gmail.com>
2024-01-01 23:37:56 +0300

    fix(controller) add const. injection to FlightScheduledJob

--- src/main/java/dev/erenuygur/flightsearchapi/controller/SecureJobController.java
+++ src/main/java/dev/erenuygur/flightsearchapi/controller/SecureJobController.java
@@ -3,16 +3,21 @@ package dev.erenuygur.flightsearchapi.controller;
 import dev.erenuygur.flightsearchapi.job.FlightScheduledJob;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/secure")
 public class SecureJobController {
 
-    private FlightScheduledJob flightScheduledJob;
+    private final FlightScheduledJob flightScheduledJob;
+
+    @Autowired
+    public SecureJobController(FlightScheduledJob flightScheduledJob) {
+        this.flightScheduledJob = flightScheduledJob;
+    }
 
     @Operation(summary = "Trigger the flight scheduled job manually.")
     @GetMapping("/triggerJob")
